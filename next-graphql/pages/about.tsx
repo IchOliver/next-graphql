12b8e2882d59e0d8af40a261063a8de34c3bc1bd
MacPc <oliver2america@gmail.com>
2021-02-04 00:29:26 -0800

    added backend

--- next-graphql/pages/about.tsx
+++ next-graphql/pages/about.tsx
@@ -1,35 +1,46 @@
 import React from "react";
-import { NextPage,NextPageContext,GetStaticProps} from "next";
-import Link from 'next/link'
-type Props ={
-    open:boolean,
-    subreddit:string
-}
-
-const About: NextPage<Props> = ({open,subreddit}) => {
-  return <nav className="text-center">
-  <ul className="flex justify-between px-4 my-4 py-1">
-    <li className="flex px-2 py-1">
-        {
-            open&& <Link href="/">
-            <a className="text-blue-500 no-underline text-sm">{subreddit}</a>
-          </Link>
-        }
-     
-    </li>
-  </ul>
-  </nav>;
+import {
+  NextPage,
+  NextPageContext,
+  GetStaticProps,
+  GetServerSideProps,
+} from "next";
+import Link from "next/link";
+type Props = {
+  open: boolean;
+  subreddit: string;
 };
 
+const About: NextPage<Props> = ({ open, subreddit }) => {
+  return (
+    <nav className="text-center">
+      <ul className="flex justify-between px-4 my-4 py-1">
+        <li className="flex px-2 py-1">
+          {open && (
+            <Link href="/">
+              <a className="text-blue-500 no-underline text-sm">{subreddit}</a>
+            </Link>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export const getStaticProps:GetStaticProps=async(context)=> {
-       
-    const subreddit='typescript'
-    return {
-      props: {
-          subreddit
-      }, // will be passed to the page component as props
-    }
-  }
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // request data fetch api
+  const request = context.req;
+  const host =
+    typeof window !== "undefined"
+      ? window.location.host || ""
+      : request.headers.host;
+  const protocol = host.includes("localhost") ? "http" : "https";
+  const subreddit = protocol;
+  return {
+    props: {
+      subreddit,
+    }, // will be passed to the page component as props
+  };
+};
 
 export default About;
